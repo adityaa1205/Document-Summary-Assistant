28f97cec4aefeea71594b6e06ce8a9c9ca6cd388
adityaa1205 <145536614+adityaa1205@users.noreply.github.com>
2025-08-29 15:46:15 +0530

    Update CORS for frontend deployment

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import cors from "cors";
 
 app.use(cors({
   origin: "*", // temporary: use "*" until frontend deployed
